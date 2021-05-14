a95e731cc166a1d74f8bf2265a97ebcf4d6cce12
Don Townsend <don@teamairship.com>
2021-05-14 11:52:16 -0400

    make some tweaks to AnimatedFade component
    - add `loop` prop - this allows for looping animations
    - rename `startingOpacity` and `endingOpacity` for consistency with AnimatedPressable
    - add JSDoc comments for props (this allows details to show up in Intellisense - see screenshot)
    - add useCurrent hook

--- src/AnimatedFade.tsx
+++ src/AnimatedFade.tsx
@@ -1,52 +1,108 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { Animated, ViewStyle } from 'react-native';
+import useCurrent from './hooks/useCurrent';
 
 type Props = {
-  fadeOnMount?: boolean;
+  /**
+   * auto-trigger fade-in?
+   */
   triggerFade?: boolean;
-  startingOpacity?: number; // 0 - 1.00
-  endingOpacity?: number; // 0 - 1.00
-  duration?: number; // ms
-  delay?: number; // ms
+
+  /**
+   * starting opacity value (0 - 1.00)
+   */
+  opacityStart?: number;
+
+  /**
+   * ending opacity value (0 - 1.00)
+   */
+  opacityEnd?: number;
+
+  /**
+   * animation duration (in ms)
+   */
+  duration?: number;
+
+  /**
+   * delay before animation start (in ms)
+   */
+  delay?: number;
   style?: ViewStyle;
+
+  /**
+   * callback function called once fadeIn animation completes
+   */
   onEnd?: () => void;
+
+  /**
+   * should this animation repeat?
+   */
+  loop?: boolean;
 };
 
 const AnimatedFade: React.FC<Props> = ({
-  fadeOnMount = true,
-  triggerFade = false,
-  startingOpacity = 0,
-  endingOpacity = 1,
+  triggerFade = true,
+  opacityStart = 0,
+  opacityEnd = 1,
   duration = 800,
   delay = 0,
+  loop = false,
   onEnd,
   style,
   children,
 }) => {
-  const opacity = useRef(new Animated.Value(startingOpacity)).current;
+  const opacity = useRef(new Animated.Value(opacityStart));
 
-  const fade = () => {
-    Animated.timing(opacity, {
-      toValue: endingOpacity,
+  const _onEnd = useCurrent(onEnd);
+  const handleEnd = useCallback(() => {
+    if (_onEnd.current) _onEnd.current();
+  }, [_onEnd]);
+
+  const fadeIn = () => {
+    Animated.timing(opacity.current, {
+      toValue: opacityEnd,
       duration,
       delay,
       useNativeDriver: true,
     }).start((status) => {
-      if (status.finished && onEnd) {
-        onEnd();
+      if (status.finished) {
+        handleEnd();
+        if (loop) fadeOut();
       }
     });
   };
 
+  const fadeOut = () => {
+    Animated.timing(opacity.current, {
+      toValue: opacityStart,
+      duration,
+      delay,
+      useNativeDriver: true,
+    }).start((status) => {
+      if (status.finished) {
+        handleEnd();
+        if (loop) fadeIn();
+      }
+    });
+  };
+
+  const _fadeIn = useCurrent(fadeIn);
+
   useEffect(() => {
-    if (fadeOnMount || triggerFade) {
-      fade();
+    if (triggerFade) {
+      _fadeIn.current();
     }
-  }, [triggerFade, fadeOnMount]);
+  }, [triggerFade, _fadeIn]);
 
   return (
-    <Animated.View style={{ opacity, ...style }}>{children}</Animated.View>
+    <Animated.View style={{ opacity: opacity.current, ...style }}>
+      {children}
+    </Animated.View>
   );
 };
 
 export default AnimatedFade;
+
+const Test = () => {
+  return <AnimatedFade></AnimatedFade>;
+};
